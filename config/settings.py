5232c1c6fb6938653f50a78886afef321eaa0ca0
wnsdud4197 <wnsdud4197@naver.com>
2021-04-16 17:44:18 +0900

    Add static root

--- config/settings.py
+++ config/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'config' / 'static',
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/' 
---
+++ config/static/js/startproject.js
@@ -0,0 +1,34 @@
+var editor = ace.edit("editor", {
+    theme: "ace/theme/tomorrow_night_eighties",
+    mode: "ace/mode/html",
+    maxLines: 30,
+    wrap: true,
+    autoScrollEditorIntoView: true,
+    fontSize: 20
+});
+var code = editor.getValue();
+var btn = document.querySelector('input');
+var txt = document.querySelector('p');
+
+
+$('.run').click(function updateBtn() {
+  if (editor.getValue().startsWith(' $ django-admin startproject')) {
+    oneline = editor.getValue().split('\r')[0];
+    projectname = oneline.split(' ')[4];
+    editor.setValue(" $ django-admin startproject ");
+    editor.insert(" $ django-admin startproject ");
+    editor.insert(projectname);
+    editor.insert("\r");
+    var myTimer = setTimeout(function() {
+      editor.insert("Something cool");
+      editor.insert("\r");
+    }, 3000);
+    txt.textContent = '';
+  } else {
+    txt.textContent = '명령어를 확인해주세요.';
+  }
+});
+
+$('.reset').click(function resetEditor() {
+  editor.setValue(" $ \r");
+});
